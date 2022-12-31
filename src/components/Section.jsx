fee9e95bdb8254cef723a08a266b9a89b5253fcf
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-31 07:50:20 -0800

    section file being styled

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -9,12 +9,8 @@ const Section = () => {
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
       <ButtonGroup>
-        <LeftButton>
-            Custom Order
-        </LeftButton>
-        <RightButton>
-            Existing Inventory
-        </RightButton>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Inventory</RightButton>
       </ButtonGroup>
     </Wrap>
   );
@@ -29,23 +25,35 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("./images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizontal
 `;
 
 const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
-`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
 const ButtonGroup = styled.div`
-    
-`
+  display: flex;
+  margin-bottom: 30px;
+`;
 
 const LeftButton = styled.div`
-   background-color : rgba(23, 26, 32, 0.8);
-   height: 40px;
-   width: 256px;
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85px;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
 
-const RightButton = styled(LeftButton)`
-    
-`
+const RightButton = styled(LeftButton)``;
